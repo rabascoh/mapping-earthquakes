79029c070588438c4f62954e6831f3e90fa452b8
rabascoh <80366530+rabascoh@users.noreply.github.com>
2021-09-12 14:10:36 -0400

    13.6.3 update

--- Earthquakes_Past7Days/static/js/logicStep3.js
+++ Earthquakes_Past7Days/static/js/logicStep3.js
@@ -36,12 +36,31 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     return {
       opacity: 1,
       fillOpacity: 1,
-      fillColor: "#ffae42",
+      fillColor: getColor(feature.properties.mag),
       color: "#000000",
       radius: getRadius(feature.properties.mag),
       stroke: true,
       weight: 0.5
     };
+  }
+  // This function determines the color of the circle based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+    if (magnitude > 5) {
+      return "#ea2c2c";
+    }
+    if (magnitude > 4) {
+      return "#ea822c";
+    }
+    if (magnitude > 3) {
+      return "#ee9c00";
+    }
+    if (magnitude > 2) {
+      return "#eecc00";
+    }
+    if (magnitude > 1) {
+      return "#d4ee00";
+    }
+    return "#98ee00";
   }
   function getRadius(magnitude) {
     if (magnitude === 0) {
@@ -49,12 +68,19 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     }
     return magnitude * 4;
   }
+// Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
-    //Make circle markers
+    // We turn each feature into a circleMarker on the map.
     pointToLayer: function(feature, latlng) {
-      console.log(data);
-      return L.circleMarker(latlng);
-  },
-  style: styleInfo
+        console.log(data);
+        return L.circleMarker(latlng);
+      },
+    // We set the style for each circleMarker using our styleInfo function.
+  style: styleInfo,
+    // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+    onEachFeature: function(feature, layer) {
+    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+  }
 }).addTo(map);
 });
